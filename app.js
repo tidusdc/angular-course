a1e950f94277ef9d0681de1d1589f5f687f5cd1e
tidusdc <supoladb@Davids-MacBook-Pro.local>
2017-01-08 22:51:09 -0500

    assigment code commit

--- app.js
+++ app.js
@@ -12,8 +12,12 @@
         $scope.stack = function() {
 
             var array = $scope.input.split(',');
+          //  $scope.message = $scope.input
 
-            if (array.length < 3) {
+            if ($scope.message == "") {
+                $scope.message = "Please enter data first"
+            }
+            else if (array.length < 3 && $scope.message !== null ) {
                 $scope.message = "Enjoy";
             } else {
                 $scope.message = "Too much!";
